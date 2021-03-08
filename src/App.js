751739117508f3033f1533137281c57eafe572ac
Asif2535 <aiqbal517@gmail.com>
2021-03-08 23:27:42 +0600

    start project with material ui

--- src/App.js
+++ src/App.js
@@ -5,27 +5,11 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Home from './components/Home/Home';
-import NotFound from './components/Home/NotFound/NotFound';
-import CountryInfo from './components/CountryInfo/CountryInfo';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/country/:countryName">
-          <CountryInfo/>
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="*">
-          <NotFound/>
-        </Route>
-      </Switch>
+      
     </Router>
   );
 }
